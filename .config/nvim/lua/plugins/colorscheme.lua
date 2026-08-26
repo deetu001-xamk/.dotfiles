6cb353661843fcc94f337d1c194d8a2178a796b8
turtsii <79640031+turtsii@users.noreply.github.com>
2026-08-26 16:56:56 +0300

    Couple changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,19 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	config = function()
-        require("gruvbox").setup({
-            transparent_mode = true
-        })
-		vim.cmd("colorscheme gruvbox")
-	end
-}
+  "folke/tokyonight.nvim",
+  lazy = false,    -- Load immediately during startup
+  priority = 1000, -- Load before all other plugins
+  config = function()
+    require("tokyonight").setup({
+      style = "night",     -- Choose: storm, moon, night, or day
+      transparent = true,  -- Disables the main background color
 
+      styles = {
+        sidebars = "transparent", -- Transparent backgrounds for tree-views/sidebars
+        floats = "transparent",   -- Transparent backgrounds for popups/floating windows
+      },
+    })
+
+    -- Load the colorscheme after configuring it
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
