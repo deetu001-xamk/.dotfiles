311c50a7097027ed013b3a4aa6ed0cf02a11b2bf
turtsii <79640031+turtsii@users.noreply.github.com>
2026-09-03 13:37:58 +0300

    Removing useless stuff

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,15 @@
+-- lua/plugins/rose-pine.lua
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,    -- Load immediately during startup
-  priority = 1000, -- Load before all other plugins
-  config = function()
-    require("tokyonight").setup({
-      style = "night",     -- Choose: storm, moon, night, or day
-      transparent = true,  -- Disables the main background color
-
-      styles = {
-        sidebars = "transparent", -- Transparent backgrounds for tree-views/sidebars
-        floats = "transparent",   -- Transparent backgrounds for popups/floating windows
-      },
-    })
-
-    -- Load the colorscheme after configuring it
-    vim.cmd([[colorscheme tokyonight]])
-  end,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        styles = {
+            transparency = true,
+        },
+    },
+    init = function()
+        vim.cmd("colorscheme rose-pine")
+    end,
 }
